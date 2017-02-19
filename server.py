a207a51f58244938435db05e86a5c831f2c120eb
Lakshay Kalbhor <lakshaykalbhor@gmail.com>
2017-02-19 12:07:17 +0530

    using sockets to get ip

--- server.py
+++ server.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template
 import os
+import socket
 
 
 app = Flask(__name__)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("google.com",80))
+IP_ADDR = (s.getsockname()[0])
+s.close()
+ 
 @app.route('/')
 def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host = '0.0.0.0') 
+   app.run(debug=True, host = '0.0.0.0') 

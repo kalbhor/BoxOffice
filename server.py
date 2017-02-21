dee0752ab5840b40d13a86d949857a4edd8de4f6
Lakshay Kalbhor <lakshaykalbhor@gmail.com>
2017-02-21 17:18:13 +0530

    Collecting files

--- server.py
+++ server.py
@@ -1,18 +1,26 @@
+#!/usr/bin/env python3
+
 from flask import Flask, render_template
 import os
 import socket
 
 
 app = Flask(__name__)
+movies = []
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("google.com",80))
 IP_ADDR = (s.getsockname()[0])
 s.close()
+
+for root, dirs, files in os.walk(".", topdown = True):
+	for file in files:
+		if file.endswith('.mp4'):
+			movies.append(os.path.join(root, file))
  
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', movies = movies)
 
 if __name__ == '__main__':
-   app.run(debug=True, host = '0.0.0.0') 
+    app.run(debug=True, host = '0.0.0.0') 

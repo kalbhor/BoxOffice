1e2c099aad9d1d9a3435591ec81aa1ce5f06cdd4
Lakshay Kalbhor <lakshaykalbhor@gmail.com>
2017-03-10 22:41:09 +0530

    Added imdb optins

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
-<p align="center">
-<img src="banner.png" width=1700px />
-<p/>
-
 ## Currently on a halt, will start building in late April 2017.
 
 
--- movietools/movietools.py
+++ movietools/movietools.py
@@ -1,14 +1,15 @@
 import requests
 import json
+import omdb
 from bs4 import BeautifulSoup
-
-YOUTUBECLASS = 'spf-prefetch'
 
 def get_trailer_url(query):
     """
     Gather all urls of the trailer for a search query
     from youtube
     """
+
+    YOUTUBECLASS = 'spf-prefetch'
     query = "{} {}".format(query,'trailer')
     html = requests.get('https://www.youtube.com/results',
                         params={'search_query': query})
@@ -20,4 +21,37 @@ def get_trailer_url(query):
                  for i in soup_section]
 
     return video_urls 
+
+def get_imdb_info(query):
+    """
+    Gathers all imdb info on a movie/tv show
+    """
+    search = omdb.search(query)
+    imdb_info = {}
+
+    try:
+        search = search[0].title
+    except IndexError:
+        print('An exception occured while searching for query')
+    else:
+        search_result = omdb.title(search)
+        imdb_info = {
+            'title' : search_result.title,
+            'cast' : search_result.actors,
+            'genre' : search_result.genre,
+            'plot' : search_result.plot,
+            'rating' : search_result.imdb_rating,
+            'poster' : search_result.poster,
+            'writer' : search_result.writer,
+            }
+    
+    return imdb_info
+
+
+
+if __name__ == '__main__':
+    query = input('> ')
+    info = get_imdb_info(query)
+    print(info)
+
 
